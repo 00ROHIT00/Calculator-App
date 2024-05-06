8d66aca95d5f0cf2eb47ca217b3d88656605f65e
00ROHIT00 <rohithr01092003@gmail.com>
2024-05-06 21:27:16 +0530

    Added the UI

---
+++ script.js
@@ -0,0 +1,32 @@
+const answerBox = document.getElementsByClassName("answerBox");
+
+const backspace = document.getElementsByClassName("backspace".innerText);
+const clear = document.getElementsByClassName("clear");
+const clearE = document.getElementsByClassName("clearE");
+const MRbtn = document.getElementsByClassName("MRbtn");
+
+const seven = document.getElementsByClassName("7");
+const eight = document.getElementsByClassName("8");
+const nine = document.getElementsByClassName("9");
+const divide = document.getElementsByClassName("divide");
+
+const four = document.getElementsByClassName("4");
+const five = document.getElementsByClassName("5");
+const six = document.getElementsByClassName("6");
+const mulitply = document.getElementsByClassName("multiply");
+
+const one = document.getElementsByClassName("1");
+const two = document.getElementsByClassName("2");
+const three = document.getElementsByClassName("3");
+const minus = document.getElementsByClassName("minus");
+
+const zero = document.getElementsByClassName("zero");
+const twoZero = document.getElementsByClassName("twoZero");
+const add = document.getElementsByClassName("add");
+const equal = document.getElementsByClassName("equal");
+
+
+function clickEvent(buttonValue) {
+    console.log(buttonValue);
+}
+
